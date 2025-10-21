4561010100fb3e43e967a0d15204e8ee1007fe93
Hanifi Demir <101357112+hanifidemir5@users.noreply.github.com>
2025-10-21 22:12:10 +0300

    Create youtubeAuth.tsx

---
+++ app/helpers/youtubeAuth.tsx
@@ -0,0 +1,20 @@
+const CLIENT_ID = process.env.NEXT_PUBLIC_YOUTUBE_CLIENT_ID!;
+const REDIRECT_URI = "http://localhost:3000";
+const AUTH_ENDPOINT = "https://accounts.google.com/o/oauth2/v2/auth";
+const RESPONSE_TYPE = "token";
+
+const SCOPES = [
+  "https://www.googleapis.com/auth/youtube.readonly",
+  "https://www.googleapis.com/auth/youtube.force-ssl"
+];
+
+export function loginWithYouTube() {
+  const state = "youtube"; // unique identifier
+  const authUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
+    REDIRECT_URI
+  )}&response_type=${RESPONSE_TYPE}&scope=${SCOPES.join(
+    " "
+  )}&include_granted_scopes=true&prompt=consent&state=${state}`;
+
+  window.location.href = authUrl;
+}

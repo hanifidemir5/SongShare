21a42d35d10e5813892e2cce6da98632e40425d0
Hanifi Demir <101357112+hanifidemir5@users.noreply.github.com>
2025-10-21 22:14:12 +0300

    Updated Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,11 +2,15 @@ type Props = {
   onSearch: (q: string) => void;
 };
 
-import { usePerson } from "../app/personContext";
+import { useSpotifyAuth } from "@/app/contexts/SpotifyAuthContext";
+import { usePerson } from "../app/contexts/personContext";
+import LoginButtons from "./LoginButtons";
+import { useYouTubeAuth } from "@/app/contexts/YoutubeAuthContext";
 
 export default function Header({ onSearch }: Props) {
   const { person, togglePerson } = usePerson();
-
+  const { isLoggedInWithSpotify, logoutWithSpotify} = useSpotifyAuth();
+  const { isLoggedInWithYouTube, logoutWithYouTube} = useYouTubeAuth();
   return (
     <header className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
       <div>
@@ -18,6 +22,8 @@ export default function Header({ onSearch }: Props) {
         placeholder="Ara: başlık / sanatçı / etiket"
         onChange={(e) => onSearch(e.target.value)}
       />
+      <LoginButtons isSpotifyLoggedIn={isLoggedInWithSpotify} isYoutubeLoggedIn={isLoggedInWithYouTube} logoutSpotify={logoutWithSpotify} logoutYoutube={logoutWithYouTube} />
+
       <button className="btn !px-3 !py-1 text-sm" onClick={togglePerson}>
         {person}
       </button>
